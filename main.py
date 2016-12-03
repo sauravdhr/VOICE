4131352124d33a3f25e134231e7cd2fb822c6972
Sergey Knyazev <sergey.n.knyazev@gmail.com>
2016-12-03 01:26:42 -0500

    wip

--- main.py
+++ main.py
@@ -108,6 +108,6 @@ def main(fastas, L, mode):
 
 if __name__ == "__main__":
     fastas = [sys.argv[1], sys.argv[2]]
-    mode = sys.argv[2] if len(sys.argv) > 3 else 'a'
-    L = float(sys.argv[3]) if len(sys.argv) > 4 else 0
+    mode = sys.argv[3] if len(sys.argv) > 3 else 'a'
+    L = float(sys.argv[4]) if len(sys.argv) > 4 else 0
     main(fastas, L, mode)
