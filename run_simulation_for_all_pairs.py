8a6d654a709acbe4eb7bf38d77cb04bc2fb64668
Andrii Melnyk <Andrii Melnyk>
2016-12-04 00:45:01 -0500

    Simulation for all pairs script update

--- run_simulation_for_all_pairs.py
+++ run_simulation_for_all_pairs.py
@@ -16,9 +16,10 @@ import itertools
 from collections import defaultdict
 import multiprocessing
 import subprocess
-from subprocess import STDOUT
+from subprocess import Popen, STDOUT
 from threading import Timer
 from time import sleep
+
 
 class RepeatedTimer(object):
     def __init__(self, interval, function, *args, **kwargs):
@@ -46,8 +47,8 @@ class RepeatedTimer(object):
         self.is_running = False
 
 def replace_completed_subprocesses(all_pairs, simulations_subprocesses, simulations_subprocesses_pairs, completed_pairs, init_range):
-    for i in range (0, init_range):
-        if simulations_subprocesses[i].poll() == 0:
+    for i in range (0, len(all_pairs)):
+        if simulations_subprocesses[i] and simulations_subprocesses[i].poll() == 0:
             # This simulation is completed, run another one
             # Before this, remove completed simulation from running pairs array and add it to completed ones
             pair_to_launch = []
@@ -56,10 +57,10 @@ def replace_completed_subprocesses(all_pairs, simulations_subprocesses, simulati
                 if list(pair) not in completed_pairs:
                     pair_to_launch = list(pair)
                     break
-            if not pair_to_launch:
-                sys.exit()
-            simulations_subprocesses_pairs[i] = pair_to_launch
-            simulations_subprocesses[i] = subprocess.Popen(['D:\Program Files\WinPython-64bit-3.4.4.5Qt5\python-3.4.4.amd64\python.exe', 'D:\bioinfmatics\from_git\voicerep\main.py', pair[0], pair[1], 'a', '100'])
+                if not pair_to_launch:
+                    sys.exit()
+                simulations_subprocesses_pairs[i] = pair_to_launch
+                simulations_subprocesses[i] = subprocess.Popen(['python3.4', 'main.py', pair[0], pair[1], 'a', '100'])
         
         
 # directory with all sequences
@@ -96,35 +97,47 @@ else:
 
 print('Number of cores to be used: ' + str(cores_to_use))
 
-subprocesses = []
-subprocesses_pairs = []
-completed_pairs = [None] * len(pairs)
+#subprocesses = [None] * len(pairs)
+#subprocesses_pairs = [None] * len(pairs)
+#completed_pairs = [None] * len(pairs)
 
-# Launch first simulations
-init_range = 0
-if (pairs_num > cores_to_use):
-    init_range = cores_to_use
-else:
-    init_range = pairs_num
+print("Number of pairs: " + str(len(pairs)))
+
+class ProcessPool(object):
+    def __init__(self, cores_to_use):
+        self.cores_to_use = cores_to_use
+        self.subprocesses = [None] * cores_to_use
+
+    def add_new_task(self, task):
+        for i in range(len(self.subprocesses)):
+            if not self.subprocesses[i]:
+                print('Launching simulation for pair: ' + task[0] + ' ' + task[1])
+                self.subprocesses[i] = Popen(['python3.4', 'main.py', task[0], task[1], 'a', '100'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                return True
+        return False
+
+    def check_results(self):
+        for i in range(len(self.subprocesses)):
+            if self.subprocesses[i]:
+                if self.subprocesses[i].poll() != None:
+                    #self.subprocesses[i].kill()
+                    self.subprocesses[i] = None
+                    #print('Finished simulation for pair: ' + task[0] + ' ' + task[1])
+
+    def wait(self):
+        for p in self.subprocesses:
+            if p:
+                p.wait()
     
-for i in range (0, init_range):
-    #for pair in pairs:    
-        current_pair = list(pairs[i])
-        #print('Launching simulation for pair: ' + current_pair[0] + ' ' + current_pair[1])
-        subprocesses_pairs.append(pairs[i])
-        p = subprocess.Popen(['D:\Program Files\WinPython-64bit-3.4.4.5Qt5\python-3.4.4.amd64\python.exe', 'D:\\bioinfmatics\\from_git\\voicerep\\main.py', pairs[i][0], pairs[i][1], 'a', '100'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        subprocesses.append(p)
-        for s in subprocesses:
-            for line in s.stderr:
-                print(line, end='')
-            for line in s.stdout:
-                print(line, end='')
+pool = ProcessPool(cores_to_use)
 
-        #p =  subprocess.Popen(['D:\Program Files\WinPython-64bit-3.4.4.5Qt5\python-3.4.4.amd64\python.exe', 'D:\bioinfmatics\from_git\voicerep\main.py', pairs[i][0], pairs[i][1], 'a', '100'], stdout=subprocess.PIPE)
+i = 0
+while i != len(pairs):
+    if pool.add_new_task(pairs[i]):
+        i += 1
+        continue
+    sleep(3)
+    pool.check_results()
+pool.wait()
 
-# Check for completed simulations every 3 seconds
-#rt = RepeatedTimer(3, replace_completed_subprocesses, pairs, subprocesses, subprocesses_pairs, completed_pairs, init_range)       
-        
 
-#for pair in pairs:
- #   main(pair, 100, 'a')
