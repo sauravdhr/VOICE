9a34d7031df22887e721a350bbdffa2171a7810e
Sergey <sergey.n.knyazev@gmail.com>
2017-06-08 18:45:31 -0400

    wip

--- run_pair_simulation.py
+++ run_pair_simulation.py
@@ -86,14 +86,14 @@ def run_simple_simulation(fastas, out_dir, simulations_count, L):
     for i, graph in enumerate(graphs):
         f = os.path.join(out_dir, fastas_basenames[i])
         prob_json_out_file[i] = f + '_prob.json'
-        prob_gexf_out_file[i] = f + '_prob.gexf'
-        dist_out_file[i] = f + '_dist.gexf'
-        network_creator.GexfExporter.export(graph.distance_graph, network_creator.GraphType.UNDIRECTED,
-                                            dist_out_file[i])
+#        prob_gexf_out_file[i] = f + '_prob.gexf'
+#        dist_out_file[i] = f + '_dist.gexf'
+#        network_creator.GexfExporter.export(graph.distance_graph, network_creator.GraphType.UNDIRECTED,
+#                                            dist_out_file[i])
         network_creator.JsonExporter.export(graph.probability_graph, network_creator.GraphType.DIRECTED,
                                             prob_json_out_file[i])
-        network_creator.GexfExporter.export(graph.probability_graph, network_creator.GraphType.DIRECTED,
-                                            prob_gexf_out_file[i])
+#        network_creator.GexfExporter.export(graph.probability_graph, network_creator.GraphType.DIRECTED,
+#                                            prob_gexf_out_file[i])
 
     simulations_out_dir = out_dir + '/simulation'
     if not os.path.exists(simulations_out_dir):
